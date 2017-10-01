6fc55facca13bc5c5d4683ba2dff4c67e2a62f6f
Wagura Maurice <wagura465@gmail.com>
2017-10-01 15:05:24 -0500

    Added more security to voucher generation

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -65,9 +65,9 @@ class Loan < ActiveRecord::Base
   # Generates a random string from a set of easily readable characters
   def generate_voucher_code(size = 9)
     charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
-    code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
+    code = (0...size).map{ charset.to_a[SecureRandom.random_number(charset.size)] }.join
     while Loan.where(voucher_code: code).exists?
-      code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
+      code = (0...size).map{ charset.to_a[SecureRandom.random_number(charset.size)] }.join
     end
     return code
   end
