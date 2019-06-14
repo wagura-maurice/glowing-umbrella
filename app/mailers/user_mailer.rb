35ffe7152e65a57ad6a6ce1f2943e715aa6f3380
Wagura Maurice <wagura465@gmail.com>
2019-06-14 09:57:03 +0300

    domain mail updated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,16 @@
 class UserMailer < ActionMailer::Base
-  default from: "info@egranary.co"
+  default from: "support@e-granary.com"
 
   def activation_needed_email(user)
     @user = user
-    @activation_url  = "https://www.egranary.co/activate/#{@user.activation_token}"
-    @login_url  = "www.egranary.co/login"
+    @activation_url  = "https://egranary.herokuapp.com/activate/#{@user.activation_token}"
+    @login_url  = "https://egranary.herokuapp.com/login"
     mail(:to => @user.email, :subject => "Welcome to eGranary!")
   end
 
   def reset_password_email(user)
     @user = user
-    @reset_url  = "https://www.egranary.co/reset_password/#{@user.reset_password_token}"
+    @reset_url  = "https://egranary.herokuapp.com/reset_password/#{@user.reset_password_token}"
     mail(:to => @user.email, :subject => "Reset your password for eGranary")
   end
 
