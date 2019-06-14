93a2dd1a8b25b33a10b392560c492e4485ca4bb7
Wagura Maurice <wagura465@gmail.com>
2019-06-14 10:03:38 +0300

    migration files generated for crop inputs models

---
+++ db/migrate/20190613085832_create_rice_inputs.rb
@@ -0,0 +1,15 @@
+class CreateRiceInputs < ActiveRecord::Migration
+	def change
+		create_table :rice_inputs do |t|
+			t.integer :kg_of_seed
+			t.integer :bags_of_dap_fertilizer
+			t.integer :bags_of_npk_fertilizer
+			t.integer :bags_of_can_fertilizer
+			t.string :agro_chem
+			t.integer :acres_planting
+			t.integer :season
+			t.belongs_to :farmer, index: true
+			t.timestamps null: false
+		end
+	end
+end
---
+++ db/migrate/20190613085846_create_black_eyed_beans_inputs.rb
@@ -0,0 +1,15 @@
+class CreateBlackEyedBeansInputs < ActiveRecord::Migration
+	def change
+		create_table :black_eyed_beans_inputs do |t|
+			t.integer :kg_of_seed
+			t.integer :bags_of_dap_fertilizer
+			t.integer :bags_of_npk_fertilizer
+			t.integer :bags_of_can_fertilizer
+			t.string :agro_chem
+			t.integer :acres_planting
+			t.integer :season
+			t.belongs_to :farmer, index: true
+			t.timestamps null: false
+		end
+	end
+end
