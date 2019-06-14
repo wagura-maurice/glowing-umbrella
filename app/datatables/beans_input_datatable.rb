fbe1be1c3b95c54186420521662dff0e4324db3f
Wagura Maurice <wagura465@gmail.com>
2019-06-14 10:06:14 +0300

    ajax datatable view files generated for crop inputs

---
+++ app/datatables/beans_input_datatable.rb
@@ -0,0 +1,64 @@
+class BeansInputDatatable < AjaxDatatablesRails::Base
+
+  include ModelSearch
+
+  def_delegator :@view, :link_to
+  def_delegator :@view, :edit_beans_report_path
+
+
+  def view_columns
+    # Declare strings in this format: ModelName.column_name
+    # or in aliased_join_table.column_name format
+    @view_columns ||= {
+      # id: { source: "User.id", cond: :eq },
+      # name: { source: "User.name", cond: :like }
+      farmer_name:         { source: "Farmer.name",                    cond: :like      , searchable: true,  orderable: true },
+      farmer_phone_number: { source: "Farmer.phone_number",            cond: :like      , searchable: true,  orderable: false },
+      farmer_association:  { source: "Farmer.association_name",        cond: :like      , searchable: true,  orderable: false },
+      reporting_time:      { source: "BeansInput.created_at",         cond: :date_range, searchable: false, orderable: true },
+      kg_of_seed_planted:  { source: "BeansInput.kg_of_seed_planted", cond: :gteq      , searchable: false, orderable: true },
+      kg_of_fertilizer:  { source: "BeansInput.kg_of_fertilizer", cond: :gteq      , searchable: false, orderable: true },
+      bags_harvested:      { source: "BeansInput.bags_harvested",     cond: :gteq      , searchable: false, orderable: true },
+      grade_1_bags:        { source: "BeansInput.grade_1_bags",       cond: :gteq      , searchable: false, orderable: true },
+      grade_2_bags:        { source: "BeansInput.grade_2_bags",       cond: :gteq      , searchable: false, orderable: true },
+      ungraded_bags:       { source: "BeansInput.ungraded_bags",      cond: :gteq      , searchable: false, orderable: true }
+    }
+  end
+
+
+  private
+
+
+  def data
+    records.map do |record|
+      {
+        # example:
+        # id: record.id,
+        # name: record.name
+        farmer_name: link_to(record.farmer.display_name, edit_beans_report_path(record)),
+        farmer_phone_number: record.farmer.phone_number,
+        farmer_association: record.farmer.association_name,
+        reporting_time: link_to(record.reporting_time, edit_beans_report_path(record)),
+        kg_of_seed_planted: record.kg_of_seed_planted,
+        kg_of_fertilizer: record.kg_of_fertilizer,
+        bags_harvested: record.bags_harvested,
+        grade_1_bags: record.grade_1_bags,
+        grade_2_bags: record.grade_2_bags,
+        ungraded_bags: record.ungraded_bags
+      }
+    end
+  end
+
+
+  def base_query
+    BeansInput.includes(:farmer).references(:farmer)
+  end
+
+
+  def get_raw_records
+    records = run_queries(BeansInput, params)
+    return records
+  end
+
+  # ==== Insert 'presenter'-like methods below if necessary
+end
---
+++ app/datatables/pigeon_peas_input_datatable.rb
@@ -0,0 +1,65 @@
+class PigeonPeasInputDatatable < AjaxDatatablesRails::Base
+
+  include ModelSearch
+
+  def_delegator :@view, :link_to
+  def_delegator :@view, :edit_pigeon_peas_input_path
+
+
+  def view_columns
+    # Declare strings in this format: ModelName.column_name
+    # or in aliased_join_table.column_name format
+    @view_columns ||= {
+      # id: { source: "User.id", cond: :eq },
+      # name: { source: "User.name", cond: :like }
+      farmer_name:         { source: "Farmer.name",                    cond: :like      , searchable: true,  orderable: true },
+      farmer_phone_number: { source: "Farmer.phone_number",            cond: :like      , searchable: true,  orderable: false },
+      farmer_association:  { source: "Farmer.association_name",        cond: :like      , searchable: true,  orderable: false },
+      reporting_time:      { source: "PigeonPeasInput.created_at",         cond: :date_range, searchable: false, orderable: true },
+      kg_of_seed:  { source: "PigeonPeasInput.kg_of_seed", cond: :gteq      , searchable: false, orderable: true },
+      bags_of_dap_fertilizer:  { source: "PigeonPeasInput.bags_of_dap_fertilizer", cond: :gteq      , searchable: false, orderable: true },
+      bags_of_npk_fertilizer:  { source: "PigeonPeasInput.bags_of_npk_fertilizer", cond: :gteq      , searchable: false, orderable: true },
+      bags_of_can_fertilizer:  { source: "PigeonPeasInput.bags_of_can_fertilizer", cond: :gteq      , searchable: false, orderable: true },
+      agro_chem:  { source: "PigeonPeasInput.agro_chem", cond: :gteq      , searchable: false, orderable: true },
+      acres_planting:  { source: "PigeonPeasInput.acres_planting", cond: :gteq      , searchable: false, orderable: true },
+      season:  { source: "PigeonPeasInput.season", cond: :gteq      , searchable: false, orderable: true }
+    }
+  end
+
+
+  def data
+    records.map do |record|
+      {
+        # example:
+        # id: record.id,
+        # name: record.name
+        farmer_name: link_to(record.farmer.display_name, edit_pigeon_peas_input_path(record)),
+        farmer_phone_number: record.farmer.phone_number,
+        farmer_association: record.farmer.association_name,
+        reporting_time: link_to(record.reporting_time, edit_pigeon_peas_input_path(record)),
+        kg_of_seed: record.kg_of_seed,
+        bags_of_dap_fertilizer: record.bags_of_dap_fertilizer,
+        bags_of_npk_fertilizer: record.bags_of_npk_fertilizer,
+        bags_of_can_fertilizer: record.bags_of_can_fertilizer,
+        agro_chem: record.agro_chem,
+        acres_planting: record.acres_planting,
+        season: record.season
+      }
+    end
+  end
+
+
+  private
+
+  def base_query
+    PigeonPeasInput.includes(:farmer).references(:farmer)
+  end
+
+
+  def get_raw_records
+    records = run_queries(PigeonPeasInput, params)
+    return records
+  end
+
+  # ==== Insert 'presenter'-like methods below if necessary
+end
